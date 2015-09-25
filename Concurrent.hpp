1c822b1a194c997a98ad7de3615388304bd9333f
Hugues Morisset <morisset.hugues@gmail.com>
2015-09-25 19:38:19 +0200

    Time aware worker

--- Concurrent.hpp
+++ Concurrent.hpp
@@ -1,6 +1,7 @@
 #ifndef __CONCURRENTHPP__
 #define __CONCURRENTHPP__
 
+#include <chrono>
 #include <functional>
 #include <future>
 #include <memory>
@@ -36,6 +37,19 @@ inline void promise_set_value(std::promise<void>& p, F& f, Tuple&& t) {
   p.set_value();
 }
 
+
+//! Time helpers
+using Clock = std::chrono::system_clock;
+using Ms = std::milli;
+using Sec = std::ratio<1>;
+
+template<class Type, class Scale>
+using Duration = std::chrono::duration<Type, Scale>;
+
+template<class Type, class Scale>
+using TimePoint = std::chrono::time_point<Clock, Duration<Type, Scale>>;
+
+
 //! A class that implement a worker thread which allow to share ressources and defer
 //! the work on that thread.
 //! @tparam Types The ressources types;
@@ -44,13 +58,17 @@ class Concurrent {
  public:
 
   //! Concurrent constructor
+  //! @param tasksSecond The maximum number of tasks that should be executed per seconds
   //! @param ts The shared ressources values
-  explicit Concurrent(Types&&... ts) : _done(false),
-  _t(std::forward_as_tuple(ts...)),
-  _thread([this]() {
-  while (!_done) {
-    _queue.pop()();
-  }}) {
+  explicit Concurrent(double tasksSecond, Types&&... ts)
+  : _done(false),
+  _t(std::forward_as_tuple(ts...)) {
+    if (tasksSecond == 0.0) {
+      _thread = std::thread(std::bind(&Concurrent::_executor, this));
+    } else {
+      _secondsPerTask = 1.0 / tasksSecond;
+      _thread = std::thread(std::bind(&Concurrent::_executorChrono, this));
+    }
   }
 
   //! Concurrent Destructor
@@ -96,7 +114,35 @@ class Concurrent {
     return _t;
   }
 
+ protected:
+  void _executor() {
+    while (!_done) {
+     _queue.pop()();
+    }
+  }
+
+  void _executorChrono() {
+    double taskTimeSec = 0.0;
+    Duration<double, Ms> timeToSleep;
+    TimePoint<double, Ms> start;
+    TimePoint<double, Ms> end;
+    start = std::chrono::system_clock::now();
+    while (!_done) {
+      _queue.pop()();
+      end = std::chrono::system_clock::now();
+      Duration<double, Ms> elapsed_milliseconds = start - end;
+      taskTimeSec = elapsed_milliseconds.count() / 1000.0;
+      timeToSleep = std::chrono::milliseconds(0);
+      if (taskTimeSec < _secondsPerTask) {
+        timeToSleep = std::chrono::milliseconds(static_cast<int>((_secondsPerTask - taskTimeSec) * 1000.0));
+        std::this_thread::sleep_for(timeToSleep);
+      }
+      start = end + timeToSleep;
+    }
+  }
+  
  private:
+  double _secondsPerTask;
   bool _done;
   std::tuple<Types...> _t;
   ConcurrencyQueue<std::function<void()>> _queue;
